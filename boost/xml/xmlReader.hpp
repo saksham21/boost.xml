d05b8ea21cec98cf9fe5188b0044776c8f84336a
saksham21 <sam21.zero@gmail.com>
2014-03-20 07:37:16 +0530

    new files added

---
+++ boost/xml/xmlReader.hpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//  must include all useful header files
+
+namespace boost
+{
+	namespace xml
+	{
+
+		template< class string_type, class string_adaptor_type>
+		class xmlReaderInterface
+		{
+			/* Interface for reading an XML document using callback. This interface allows an application to set and query features and properties in the parser, to register event handlers for document processing, and to initiate a document parse.	*/
+		};
+	} // namespace xml
+} // namespace boost
