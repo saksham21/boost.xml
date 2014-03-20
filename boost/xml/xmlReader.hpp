5b9f68b27753d52d4a5b4f24702f9cc1ee12197e
saksham21 <sam21.zero@gmail.com>
2014-03-20 07:51:19 +0530

    parsing interface

--- boost/xml/xmlReader.hpp
+++ boost/xml/xmlReader.hpp
@@ -11,7 +11,27 @@ namespace boost
 		template< class string_type, class string_adaptor_type>
 		class xmlReaderInterface
 		{
-			/* Interface for reading an XML document using callback. This interface allows an application to set and query features and properties in the parser, to register event handlers for document processing, and to initiate a document parse.	*/
+		public:
+			
+			/* Interface for reading an XML document using callback. This interface allows an application
+			to set and query features and properties in the parser, to register event handlers for document
+			processing, and to initiate a document parse.	*/
+
+			/* Various types of functions and features of the xml Document parser like errorHandling and 
+			contentHandling will also be impleneted here */
+
+			void parse(InputSource *abc)
+			{
+				/* here the document to be parsed will be finally called and from here wrapper function of the
+				particular backend will handle the parsing calls. */
+
+				/* InputSource tells us whether the input stream is byte-stream or character-stream or whether
+				the entire document is given as query or it is a buffer stream. The "souce abc" will be decided
+				correspondingly. */
+			}
+
+		private:
+
 		};
 	} // namespace xml
 } // namespace boost
